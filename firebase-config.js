d4f8b22ca33af0b88b9a6a72530b5f7b699e822f
David Gaynor <runnedrun@gmail.com>
2016-10-31 09:33:41 -0700

    Classadoo autocommit

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,7 @@
 var firebaseConfig = {
-  apiKey: "AIzaSyB4n3T-wB2G2UnIrMUU3hC7SqoXhPEb4L0",
-  authDomain: "classadoo-dev.firebaseapp.com",
-  databaseURL: "https://classadoo-dev.firebaseio.com"	
+  apiKey: "",
+  authDomain: "domain-here.firebaseapp.com",
+  databaseURL: "https://domain-here.firebaseio.com"	
 };
 
 firebase.initializeApp(firebaseConfig);
